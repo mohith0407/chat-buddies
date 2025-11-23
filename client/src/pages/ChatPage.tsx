1583f1dad9cdfdaece065e5042aa3682661002d7
mohith0407 <mohith_g@srmap.edu.in>
2025-11-23 13:28:09 +0530

    Updated mobile screen

--- client/src/pages/ChatPage.tsx
+++ client/src/pages/ChatPage.tsx
@@ -7,15 +7,15 @@ const ChatPage = () => {
   const { user, isProfileOpen, selectedChat } = useChatState();
 
   return (
-    <div className="w-full h-screen flex overflow-hidden bg-gray-950">
+    <div className="w-full h-dvh flex overflow-hidden bg-gray-950">
       {user && <Sidebar />}
       
-      <div className={`flex-1 flex h-full relative ${isProfileOpen ? "mr-0" : ""}`}>
+      <div className={`flex-1 flex flex-col h-full min-w-0 relative ${isProfileOpen ? "mr-0" : ""}`}>
         {user && <ChatBox />}
       </div>
       
       {user && selectedChat && isProfileOpen && (
-        <div className="absolute inset-y-0 right-0 z-50 md:static md:z-0">
+        <div className="absolute inset-y-0 right-0 z-50 md:static md:z-0 ">
           <InfoSidebar />
         </div>
       )}
